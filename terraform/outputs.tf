696bba5c6650a20d11ce2dc36980dd71dcc2e0ad
Josh Lindsey <joshua.s.lindsey@gmail.com>
2017-06-18 23:33:16 -0400

    update outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
-output "ip" {
+output "instance_ip" {
+  value = "${digitalocean_droplet.minecraft.ipv4_address}"
+}
+
+output "floating_ip" {
   value = "${digitalocean_floating_ip.minecraft.ip_address}"
 }
